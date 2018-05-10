27e508dbd836830851945a0f75fabb88877f1214
bill bittner <bittner.w@gmail.com>
2018-05-10 00:00:36 -0700

    added multisite page and react route

--- src/lib/app.js
+++ src/lib/app.js
@@ -7,12 +7,14 @@ const customizedApp = ({components, containers, pages}) => {
     const LoginPage = pages.LoginPage || require('./pages/LoginPage').default;
     const ShowPage = pages.ShowPage || require('./pages/ShowPage').default;
     const FourOhFourPage = pages.FourOhFourPage || require('./pages/FourOhFourPage').default;
+    const MultisitePage = require('./pages/MultisitePage').default;
     return () => {
         return (
             <Switch>
                 <Route exact path='/' component={HomePage}/>
                 <Route exact path='/about' component={AboutPage}/>
                 <Route exact path='/login' component={LoginPage}/>
+                <Route exact path='/multisite' component={MultisitePage}/>
                 <Route exact path='/:identifier/:claim' component={ShowPage}/>
                 <Route exact path='/:claim' component={ShowPage}/>
                 <Route component={FourOhFourPage}/>
