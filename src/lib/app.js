3ef2bc27d469aa4167b4f2a5f09a18e97d1dd054
bill bittner <bittner.w@gmail.com>
2018-05-13 11:47:18 -0700

    removed use of custom views in App and NavBar

--- src/lib/app.js
+++ src/lib/app.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-const customizedApp = ({components, containers, pages}) => {
-    const HomePage = pages.HomePage || require('./pages/HomePage').default;
-    const AboutPage = pages.AboutPage || require('./pages/AboutPage').default;
-    const LoginPage = pages.LoginPage || require('./pages/LoginPage').default;
-    const ShowPage = pages.ShowPage || require('./pages/ShowPage').default;
-    const FourOhFourPage = pages.FourOhFourPage || require('./pages/FourOhFourPage').default;
-    const MultisitePage = require('./pages/MultisitePage').default;
-    return () => {
-        return (
-            <Switch>
-                <Route exact path='/' component={HomePage}/>
-                <Route exact path='/about' component={AboutPage}/>
-                <Route exact path='/login' component={LoginPage}/>
-                <Route exact path='/multisite' component={MultisitePage}/>
-                <Route exact path='/:identifier/:claim' component={ShowPage}/>
-                <Route exact path='/:claim' component={ShowPage}/>
-                <Route component={FourOhFourPage}/>
-            </Switch>
-        );
-    };
+import HomePage  from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import LoginPage from './pages/LoginPage';
+import ShowPage from './pages/ShowPage';
+import FourOhFourPage from './pages/FourOhFourPage';
+import MultisitePage from './pages/MultisitePage';
+
+const customizedApp = () => {
+    return (
+        <Switch>
+            <Route exact path='/' component={HomePage}/>
+            <Route exact path='/about' component={AboutPage}/>
+            <Route exact path='/login' component={LoginPage}/>
+            <Route exact path='/multisite' component={MultisitePage}/>
+            <Route exact path='/:identifier/:claim' component={ShowPage}/>
+            <Route exact path='/:claim' component={ShowPage}/>
+            <Route component={FourOhFourPage}/>
+        </Switch>
+    );
 };
 
 export default customizedApp;
