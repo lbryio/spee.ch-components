4b51b8d27e82c03cbde9dcb5a51bf916b3ef4c31
bill bittner <bittner.w@gmail.com>
2018-05-13 22:03:58 -0700

    updated App and Nav Bar to use aliased paths

--- src/lib/app.js
+++ src/lib/app.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import HomePage  from './pages/HomePage';
-import AboutPage from '@pages/AboutPage/index.js';
-import LoginPage from './pages/LoginPage';
-import ShowPage from './pages/ShowPage';
-import FourOhFourPage from './pages/FourOhFourPage';
-import MultisitePage from './pages/MultisitePage';
+import HomePage  from '@pages/HomePage';
+import AboutPage from '@pages/AboutPage';
+import LoginPage from '@pages/LoginPage';
+import ShowPage from '@pages/ShowPage';
+import FourOhFourPage from '@pages/FourOhFourPage';
+import MultisitePage from '@pages/MultisitePage';
 
 const customizedApp = () => {
     return (
