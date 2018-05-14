e728f2b64cfc5638a17fed8ad1568ec80b4a9347
bill bittner <bittner.w@gmail.com>
2018-05-13 22:25:32 -0700

    removed the components from the root export

--- src/lib/index.jsx
+++ src/lib/index.jsx
@@ -3,89 +3,10 @@ import App from './app';
 import Reducers from './reducers';
 import Sagas from './sagas';
 import Actions from './actions';
-// containers
-import AboutPage from './pages/AboutPage';
-import ErrorPage from './pages/ErrorPage';
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import ShowPage from './pages/ShowPage';
-import AssetDisplay from './containers/AssetDisplay';
-import AssetInfo from './containers/AssetInfo';
-import AssetTitle from './containers/AssetTitle';
-import ChannelClaimsDisplay from './containers/ChannelClaimsDisplay';
-import ChannelCreateForm from './containers/ChannelCreateForm';
-import ChannelLoginForm from './containers/ChannelLoginForm';
-import ChannelSelect from './containers/ChannelSelect';
-import Dropzone from './containers/Dropzone';
-import FourOhFourPage from './pages/FourOhFourPage';
-import NavBar from './containers/NavBar';
-import PublishDetails from './containers/PublishDetails';
-import PublishDisabledMessage from './containers/PublishDisabledMessage';
-import PublishMetadataInputs from './containers/PublishMetadataInputs';
-import PublishStatus from './containers/PublishStatus';
-import PublishThumbnailInput from './containers/PublishThumbnailInput';
-import PublishTitleInput from './containers/PublishTitleInput';
-import PublishTool from './containers/PublishTool';
-import PublishUrlInput from './containers/PublishUrlInput';
-import ShowAssetDetails from './containers/ShowAssetDetails';
-import ShowAssetLite from './containers/ShowAssetLite';
-import ShowChannel from './containers/ShowChannel';
-// components
-import ActiveStatusBar from './components/ActiveStatusBar';
-import AssetPreview from './components/AssetPreview';
-import ExpandingTextArea from './components/ExpandingTextArea';
-import GAListener from './components/GAListener';
-import InactiveStatusBar from './components/InactiveStatusBar';
-import Logo from './components/Logo';
-import NavBarChannelOptionsDropdown from './components/NavBarChannelOptionsDropdown';
-import ProgressBar from './components/ProgressBar';
-import PublishPreview from './components/PublishPreview';
-import PublishUrlMiddleDisplay from './components/PublishUrlMiddleDisplay';
-import SEO from './components/SEO';
 
-
-// can it be a function and pass in the config like this?,
-// or should all the configs just come from the store (assuming the initial config can be used to config the store)?
 module.exports = {
     App,
     Reducers,
-    Sagas, // includes all the sagas
-    Actions, // includes all the actions
-    AboutPage,
-    ErrorPage,
-    HomePage,
-    LoginPage,
-    ShowPage,
-    AssetDisplay,
-    AssetInfo,
-    AssetTitle,
-    ChannelClaimsDisplay,
-    ChannelCreateForm,
-    ChannelLoginForm,
-    ChannelSelect,
-    Dropzone,
-    FourOhFourPage,
-    NavBar,
-    PublishDetails,
-    PublishDisabledMessage,
-    PublishMetadataInputs,
-    PublishStatus,
-    PublishThumbnailInput,
-    PublishTitleInput,
-    PublishTool,
-    PublishUrlInput,
-    ShowAssetDetails,
-    ShowAssetLite,
-    ShowChannel,
-    ActiveStatusBar,
-    AssetPreview,
-    ExpandingTextArea,
-    GAListener,
-    InactiveStatusBar,
-    Logo,
-    NavBarChannelOptionsDropdown,
-    ProgressBar,
-    PublishPreview,
-    PublishUrlMiddleDisplay,
-    SEO,
+    Sagas,
+    Actions,
 };
